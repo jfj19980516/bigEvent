2f7ab55213d9c8df9cc0eae3d712b7edeb4f4d15
mayun-jfj <jfjqwer0@163.com>
2020-05-29 16:36:54 +0800

    删除和修改分类的时候判断一些id

--- routers/category.js
+++ routers/category.js
@@ -4,9 +4,61 @@
 // 导出路由
 
 const express = require('express');
-
+const path = require('path');
+const db = require(path.join(__dirname, '../utils', 'db'));
 const router = express.Router();
 
 // -----------写接口--------------
+
+// ------------获取文章分类列表--------------
+router.get('/cates', async (req, res) => {
+    let r = await db('select * from category');
+    if (r) {
+        res.send({
+            status: 0,
+            message: '获取文章分类列表成功',
+            data: r
+        });
+    } else {
+        res.send({ status: 1, message: '获取文章分类列表失败' })
+    }
+});
+
+// ------------新增文章分类列表--------------
+router.post('/addcates', async (req, res) => {
+    let r = await db('insert into category set ?', req.body);
+    if (r && r.affectedRows > 0) {
+        res.send({ status: 0, message: '添加分类成功' });
+    } else {
+        res.send({ status: 1, message: '添加分类失败' });
+    }
+});
+// ------------根据ID删除文章分类--------------
+router.get('/deletecate/:id', async (req, res) => {
+    let id = req.params.id; //先拿到id
+    if (id != req.user.id) {
+        return res.send({ status: 1, message: 'id错误，请重新登录' });
+    }
+    let r = await db('delete from category where id=?', id);
+    if (r && r.affectedRows > 0) {
+        res.send({ status: 0, message: '删除分类成功' });
+    } else {
+        res.send({ status: 0, message: '删除分类成功' });
+    }
+});
+
+// ------------根据ID更新文章分类--------------
+router.post('/updatecate', async (req, res) => {
+    if (req.body.Id != req.user.id) {
+        return res.send({ status: 1, message: 'id错误，请重新登录' });
+    }
+    let r = await db('update category set ? where Id=?', [req.body, req.body.Id]);
+    if (r && r.affectedRows > 0) {
+        res.send({ status: 0, message: '更新成功' });
+    } else {
+        res.send({ status: 0, message: '更新失败' });
+    }
+});
+
 
 module.exports = router; 
--- routers/user.js
+++ routers/user.js
@@ -4,13 +4,73 @@
 // 导出路由
 
 const express = require('express');
+const path = require('path');
+const db = require(path.join(__dirname, '../utils', 'db'));
+const utility = require('utility');
 const router = express.Router();
 
 // ---------------------     写接口      ---------------------------
 // 获取用户信息接口
-router.get('/userinfo', (req, res) => {
+router.get('/userinfo', async (req, res) => {
     // 
-    console.log(req.user);
+    let r = await db('select id,username,nickname,email,user_pic from user where id=?', req.user.id);
+    if (r && r.length > 0) {
+        res.send({
+            status: 0,
+            maessage: "获取用户信息成功",
+            data: r[0]
+        });
+    } else {
+        res.send({ status: 1, maessage: "获取用户信息失败!" });
+    }
+
+});
+
+// ---------------更新密码接口-----------------
+router.post('/updatepwd', async (req, res) => {
+    // 先获取新密码和旧密码
+    let oldPwd = utility.md5(req.body.oldPwd);
+    let newPwd = utility.md5(req.body.newPwd);
+    // 1.判断新旧密码是否一致
+    if (oldPwd == newPwd) {
+        return res.send({ status: 1, maessage: "新旧密码不能一致" });
+    }
+    // 2.先检查原密码是否正确
+    let r1 = await db('select * from user where password=? and id=?', [oldPwd, req.user.id]);
+    if (r1.length === 0 || r1 === undefined) {
+        return res.send({ status: 1, maessage: "原密码错误" });
+    }
+    // 3.更新密码 
+    let r2 = await db('update user set password=? where id=?', [newPwd, req.user.id]);
+    if (r2 && r2.affectedRows > 0) {
+        res.send({ status: 0, maessage: "更新密码成功" });
+    } else {
+        res.send({ status: 1, maessage: "更新密码失败" });
+    }
+});
+
+// --------------更换头像---------------------
+router.post('/update/avatar', async (req, res) => {
+    let r = await db('update user set user_pic=? where id=?', [req.body.avatar, req.user.id]);
+    if (r && r.affectedRows > 0) {
+        res.send({ status: 0, maessage: "更换头像成功" });
+    } else {
+        res.send({ status: 1, maessage: "更换头像失败" });
+    }
+});
+
+// --------------更新用户信息---------------------------
+router.post('/userinfo', async (req, res) => {
+    // 判断一下提交的id是否和当前用户的id一致
+    if (req.body.id != req.user.id) {
+        return res.send({ status: 0, maessage: 'id错误请重新登录' });
+    }
+    let r = await db('update user set ? where id=?', [req.body, req.user.id]);
+    if (r && r.affectedRows > 0) {
+        res.send({ status: 0, maessage: '更新用户信息成功' });
+    } else {
+        res.send({ status: 1, maessage: '更新用户信息失败' });
+    }
 });
 
 module.exports = router; 
